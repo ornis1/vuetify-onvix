4ae6ac15393707bfdcd70f8509597cc84e33ea91
ornis1 <fastmarat23@gmail.com>
2019-11-18 16:02:18 +0300

    Create TheLogin component

---
+++ src/views/TheLogin.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card width="400" class="d-flex flex-column align-center mx-auto mt-5 pb-2">
+    <v-card-text>
+      <v-img
+        contain
+        height="400"
+        src="https://i.pinimg.com/originals/92/d7/ca/92d7ca41cde458798778eca8e228c837.jpg"
+      ></v-img>
+    </v-card-text>
+    <v-card-actions>
+      <v-form>
+        <v-text-field
+          v-model="field1"
+          background-color="grey darken-4"
+          autofocus
+          prepend-inner-icon="mdi-account-circle"
+          label="Имя"
+          solo
+          required
+        ></v-text-field>
+
+        <v-text-field
+          background-color="grey darken-4"
+          prepend-inner-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye':'mdi-eye-off'"
+          :type="showPassword ?'text' :  'password'"
+          @click:append="showPassword =! showPassword"
+          v-model="field2"
+          solo
+          label="Пароль"
+          required
+        ></v-text-field>
+        <v-checkbox v-model="check" color="primary" label="Зарегестрироваться"></v-checkbox>
+        <v-btn block color type="submit">{{!check ? 'Войти на сайт': 'Зарегестрироваться'}}</v-btn>
+      </v-form>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "TheLogin",
+  data() {
+    return {
+      field1: "",
+      field2: "",
+      check: false,
+      showPassword: false
+    };
+  }
+};
+</script>
