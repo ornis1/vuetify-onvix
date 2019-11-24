abf2b2d52e4c0a18daf6a9d4ea57ad9401351ecf
ornis1 <fastmarat23@gmail.com>
2019-11-24 19:46:55 +0300

    Update footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,3 +1,31 @@
 <template>
-  <v-footer>footer</v-footer>
+  <v-footer class="pa-0">
+    <v-container class="pa-0">
+      <v-row justify="space-between">
+        <v-col align-self="center" md="4">
+          <v-btn text height="60" :ripple="false">
+            <img src="@/assets/Logo.svg" />
+          </v-btn>
+        </v-col>
+        <v-col class="text-center" align-self="center" md="4">
+          <span>© 2016–{{year}} «Onvix»</span>
+        </v-col>
+        <v-col class="text-end" md="4">
+          <v-btn small text>О проекте</v-btn>
+          <v-btn small text>Контакты</v-btn>
+          <v-btn small text>Пользовательское соглашение</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
+<script>
+export default {
+  name: 'TheFooter',
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
