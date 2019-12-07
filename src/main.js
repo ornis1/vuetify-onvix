27f9aca767cae8c78e21c58499f844b476773f94
ornis1 <fastmarat23@gmail.com>
2019-12-07 19:35:28 +0300

    Add VueMeta import in main sript file

--- src/main.js
+++ src/main.js
@@ -7,11 +7,20 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
+import router from '@/router/router.js';
+import VueMeta from 'vue-meta';
+
+Vue.use(VueMeta, {
+  // optional pluginOptions
+  refreshOnceOnNavigation: true,
+});
 
 Vue.use(Vuex);
 Vue.config.productionTip = false;
+Vue.config.performance = true;
 
 new Vue({
+  router,
   vuetify,
   store,
   created() {
